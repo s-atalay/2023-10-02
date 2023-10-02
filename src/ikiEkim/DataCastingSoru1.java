9a2d33c138c2832d034cc80548bb9cc48285fae4
s-atalay <simgeeokurlar@gmail.com>
2023-10-02 21:47:53 +0300

    RakamlarToplami

--- src/ikiEkim/DataCastingSoru1.java
+++ src/ikiEkim/DataCastingSoru1.java
@@ -1,2 +1,23 @@
-package ikiEkim;public class DataCastingSoru1 {
+package ikiEkim;
+
+import java.util.Scanner;
+
+public class DataCastingSoru1 {
+
+    /*
+    Kullanicidan double olarak alinan 3 degerin ortalamsini int olarak yazdıralım
+     */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen 3 tamsayı giriniz");
+
+        double sayi1 = scan.nextDouble();
+        double sayi2 = scan.nextDouble();
+        double sayi3 = scan.nextDouble();
+
+        int toplam = (int) (sayi1+sayi2+sayi3);
+
+        System.out.println("Toplam = "+ toplam);
+    }
 }
